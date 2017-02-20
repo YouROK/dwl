7da3d7e02f377258f691d3e511399a93981fab0f
YouROK <8YouROK8@mail.ru>
2017-02-20 11:15:24 +0300

    refactor

--- src/dwl/progress/progress.go
+++ src/dwl/progress/progress.go
@@ -1,77 +1,58 @@
 package progress
 
 import (
-	"fmt"
 	"time"
 )
 
-type DownloadProgress struct {
-	From int64 `json:"From"`
-	To   int64 `json:"To"`
-	Pos  int64 `json:"Pos"`
+type Progress []DownloadProgress
 
-	BytesLoaded uint64    `json:"-"`
-	Speed       uint64    `json:"-"`
-	MiddleSpeed uint64    `json:"-"`
-	LastTime    time.Time `json:"-"`
-	StartTime   time.Time `json:"-"`
+func (p Progress) GetSpeed() (uint64, uint64) {
+	var speed uint64 = 0
+	var mspeed uint64 = 0
 
-	ConnectTime time.Duration `json:"-"`
-	IsLoading   bool          `json:"-"`
-}
-
-func (s *DownloadProgress) StartSpeed() {
-	s.Speed = 0
-	s.MiddleSpeed = 0
-	s.BytesLoaded = 0
-	s.StartTime = time.Now()
-	s.LastTime = time.Now()
-}
-
-func (s *DownloadProgress) EndSpeed() {
-	s.Speed = 0
-	s.MiddleSpeed = 0
-	s.BytesLoaded = 0
-}
-
-func (s *DownloadProgress) GetSpeed() (uint64, uint64) {
-	return s.Speed, s.MiddleSpeed
-}
-
-func (s *DownloadProgress) MessureSpeed(realc int) {
-	s.BytesLoaded += uint64(realc)
-
-	//	if !s.Complete() && s.BytesLoaded > 0 {
-	delta := time.Since(s.StartTime).Seconds()
-	if time.Since(s.LastTime).Seconds() > 0.1 {
-		s.LastTime = time.Now()
-		s.Speed = uint64(float64(s.BytesLoaded) / delta)
-		s.MiddleSpeed = (s.MiddleSpeed + s.Speed) / 2
-	}
-	if time.Since(s.StartTime).Seconds() > 2 {
-		s.StartTime = time.Now()
-		s.LastTime = time.Now()
-		s.BytesLoaded = 0
-	}
-	//	}
-}
-
-func (s *DownloadProgress) Complete() bool {
-	return s.Pos >= s.To && s.To > 0
-}
-
-func (s *DownloadProgress) GetPercent() int {
-	if s.To < 1 {
-		return -1
+	for _, dp := range p {
+		s, ms := dp.GetSpeed()
+		speed += s
+		mspeed += ms
 	}
 
-	loaded := s.Pos - s.From
-	all := s.To - s.From
-
-	return int(loaded * 100 / all)
+	return speed, mspeed
 }
 
-func (s DownloadProgress) String() string {
-	_, sm := s.GetSpeed()
-	return fmt.Sprintf("P: %v %v %v %v%% %v/s", s.From, s.Pos, s.To, s.GetPercent(), ByteSize(uint64(sm)))
+func (p Progress) GetPercent() int {
+	prc := 0
+	for _, dp := range p {
+		prc += dp.GetPercent()
+	}
+	return prc / len(p)
+}
+
+func (p Progress) GetAverageConnTime() time.Duration {
+	var ret time.Duration
+	count := 0
+	for _, dp := range p {
+		if dp.ConnectTime > 0 {
+			count++
+			ret += dp.ConnectTime
+		}
+	}
+	return ret / time.Duration(count)
+}
+
+func (p Progress) Complete() bool {
+	for _, dp := range p {
+		if !dp.Complete() {
+			return false
+		}
+	}
+	return true
+}
+
+func (p Progress) IsLoading() bool {
+	for _, dp := range p {
+		if dp.IsLoading {
+			return true
+		}
+	}
+	return false
 }
