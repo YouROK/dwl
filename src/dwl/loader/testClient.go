6ad7c529b13b67dd63ddbff7a67553084cf5451f
YouROK <8YouROK8@mail.ru>
2017-02-20 11:14:53 +0300

    change

--- src/dwl/loader/testClient.go
+++ src/dwl/loader/testClient.go
@@ -7,6 +7,8 @@ import (
 	"time"
 )
 
+var allsize int64 = 10 * 1024 * 1024
+
 type TestClient struct {
 	pos, off int64
 	wait     time.Duration
@@ -16,53 +18,45 @@ func NewTestClient(sets *settings.Settings, off int64) *TestClient {
 	t := new(TestClient)
 	t.off = off
 	t.pos = off
-	t.wait = time.Nanosecond * time.Duration(rand.Intn(2000))
-	//	fmt.Println("*** New client:", sets.Url, off)
+	t.wait = time.Nanosecond * time.Duration(rand.Intn(5)+int(off/1000))
 	return t
 }
 
 func (c *TestClient) Connect() error {
-	//	fmt.Println("Connect")
-	time.Sleep(time.Millisecond * time.Duration(rand.Intn(2000)))
+	//	time.Sleep(time.Millisecond * time.Duration(rand.Intn(1000)))
 	return nil
 }
 func (c *TestClient) Close() error {
-	//	fmt.Println("Close")
 	return nil
 }
 func (c *TestClient) Read(buf []byte) (n int, err error) {
-	//	fmt.Println("Read:", len(buf))
-	//	defer fmt.Println("Readed:", n)
-	allsize := c.GetAllSize()
+	as := c.GetAllSize()
+
+	if c.wait > 0 {
+		time.Sleep(c.wait * time.Duration(len(buf)))
+	}
 
 	for i := 0; i < len(buf); i++ {
-		time.Sleep(c.wait)
-		buf[i] = byte(c.pos * 255 / allsize)
+		buf[i] = byte(c.pos * 255 / as)
 		c.pos++
-		if c.pos >= allsize {
+		if c.pos >= as {
 			return i, io.EOF
 		}
 	}
 	return len(buf), nil
 }
 func (c *TestClient) IsConnected() bool {
-	//	fmt.Println("Is connected")
 	return true
 }
 func (c *TestClient) GetLastError() error {
-	//	fmt.Println("Get last err")
 	return nil
 }
 func (c *TestClient) GetAllSize() int64 {
-	var allsize int64 = 5 * 1024 * 1024
-	//	fmt.Println("Get all size:", allsize)
 	return allsize
 }
 func (c *TestClient) Pos() int64 {
-	//	fmt.Println("Pos:", c.pos)
 	return c.pos
 }
 func (c *TestClient) Off() int64 {
-	//	fmt.Println("Off:", c.off)
 	return c.off
 }
